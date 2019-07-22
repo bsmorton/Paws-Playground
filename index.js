65c061196f6d0eb16c9df4825ecfbad7efaf80b3
bsmorton <bsmorton@uci.edu>
2019-07-21 18:05:51 -0700

    Add files via upload
    added:
    -passport authentication

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Dogpark     = require("./models/dogpark"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Dogpark       = require("./models/dogpark"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
     
 
 mongoose.connect("mongodb://localhost:27017/paws_playground",{ useNewUrlParser: true });
@@ -12,6 +15,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 // seedDB();
+
+
+// Passport Configuration
+app.use(require("express-session")({
+    secret:            "Krang Rocks",
+    resave:            false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 
 app.get("/", function(req, res){
     res.render("home");
@@ -31,7 +53,7 @@ app.get("/dogparks", function(req, res){
     });
 });
 
-app.get("/dogparks/new", function(req, res) {
+app.get("/dogparks/new", isLoggedIn, function(req, res) {
     res.render("dogparks/new");
 });
 
@@ -45,7 +67,7 @@ app.get("/dogparks/:id", function(req, res){
     });
 });
 
-app.post("/dogparks", function(req, res){
+app.post("/dogparks", isLoggedIn, function(req, res){
     var name          = req.body.name,
         address       = req.body.address,
         image         = req.body.image,
@@ -67,7 +89,7 @@ app.post("/dogparks", function(req, res){
 // COMMENTS ROUTES
 // ===================
 
-app.get("/dogparks/:id/comment/new", function(req, res){
+app.get("/dogparks/:id/comment/new", isLoggedIn, function(req, res){
     Dogpark.findById(req.params.id, function(err, dogpark){
         if(err){
             console.log(err);
@@ -77,7 +99,7 @@ app.get("/dogparks/:id/comment/new", function(req, res){
     });
 });
 
-app.post("/dogparks/:id/comment", function(req, res){
+app.post("/dogparks/:id/comment", isLoggedIn, function(req, res){
     Dogpark.findById(req.params.id, function(err, dogpark){
         if(err){
             console.log(err);
@@ -95,6 +117,54 @@ app.post("/dogparks/:id/comment", function(req, res){
         }
     });
 });
+
+
+// ===================
+// AUTH ROUTES
+// ===================
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/dogparks");
+        })
+    });
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/dogparks",
+        failureRedirect: "/login"
+    
+    }), function(req, res) {});
+    
+    
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/dogparks");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The PawsPlayground Server Has Started");
